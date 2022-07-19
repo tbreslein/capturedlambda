49f114218202333d57cca8afa96024802898f81d
tbreslein <tommy.breslein@protonmail.com>
2022-07-19 20:33:40 +0200

    ENH|add and setup deps

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+    daisyui: {
+        themes: [
+        {
+            mytheme: {
+            primary: "#edddb8",
+            secondary: "#dcc080",
+            accent: "#dfb316",
+            neutral: "#292929",
+            "base-100": "#3d3d3d",
+            info: "#83a598",
+            success: "#b8bb26",
+            warning: "#d3869b",
+            error: "#f87272",
+            },
+        },
+        ],
+    },
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
