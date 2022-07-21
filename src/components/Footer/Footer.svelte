34447f1ee84b10fe4233c3ebe44071647c4038f4
tbreslein <tommy.breslein@protonmail.com>
2022-07-21 15:18:16 +0200

    FIX|styling, spacing and centering in particular

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -5,9 +5,7 @@
 <div class="p-24">
   <p />
 </div>
-<footer
-  class="fixed bottom-0 footer footer-center p-4 bg-base-300 text-base-content"
->
+<footer class="fixed bottom-0 footer footer-center p-2 bg-base-300">
   <div class="grid grid-flow-col gap-4 text-secondary">
     <a href="https://twitter.com/HedShodd" class={buttonClasses}> Twitter </a>
     <a href="https://github.com/tbreslein" class={buttonClasses}> Github </a>
@@ -15,5 +13,7 @@
       Reddit
     </a>
   </div>
-  <p>Copyright © 2022 - All right reserved by Tommy Breslein</p>
+  <p class="text-base-content">
+    Copyright © 2022 - All right reserved by Tommy Breslein
+  </p>
 </footer>
