732122ef7777c3d9ceeb882af2430f04967c96d4
tbreslein <tommy.breslein@protonmail.com>
2022-07-21 14:42:04 +0200

    ENH|impl Footer and Header

---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const buttonClasses = "btn btn-xs btn-ghost";
+</script>
+
+<div class="p-24">
+  <p />
+</div>
+<footer
+  class="fixed bottom-0 footer footer-center p-4 bg-base-300 text-base-content"
+>
+  <div class="grid grid-flow-col gap-4 text-secondary">
+    <a href="https://twitter.com/HedShodd" class={buttonClasses}> Twitter </a>
+    <a href="https://github.com/tbreslein" class={buttonClasses}> Github </a>
+    <a href="https://www.reddit.com/user/Hedshodd" class={buttonClasses}>
+      Reddit
+    </a>
+  </div>
+  <p>Copyright © 2022 - All right reserved by Tommy Breslein</p>
+</footer>
---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import MainIcon from "./MainIcon.svelte";
+  const buttonClasses = "btn-ghost text-secondary";
+</script>
+
+<div class="fixed inset-x-0 top-0 navbar h-8 bg-neutral">
+  <MainIcon />
+  <div class="flex-none">
+    <ul class="menu menu-horizontal p-0">
+      <li>
+        <a href="/" class={buttonClasses}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>Home</a
+        >
+        <a href="/about" class={buttonClasses}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>About</a
+        >
+        <a href="/contact" class={buttonClasses}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>Contact</a
+        >
+      </li>
+    </ul>
+  </div>
+</div>
+<div>
+  <p class="p-12 w-9/12" />
+</div>
