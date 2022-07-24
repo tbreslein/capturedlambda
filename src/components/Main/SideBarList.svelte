715832d9072795998883a844a829e5bc966be720
tbreslein <tommy.breslein@protonmail.com>
2022-07-24 20:11:09 +0200

    TWK|tweak styling for sidebar

--- src/components/Main/SideBarList.svelte
+++ src/components/Main/SideBarList.svelte
@@ -4,11 +4,13 @@
   export let postsByYear: PostsByYear[];
 </script>
 
-<ul class="menu bg-base-100 w-56 p-2 rounded-box">
+<ul class="menu bg-base-100 p-1">
   {#each postsByYear as { year, posts } (year)}
     <li class="menu-title"><span>{year}</span></li>
     {#each posts as post (post.title)}
-      <li class="text-xs">{post.title}</li>
+      <li class="text-sm">
+        <a class="link-hover" href={post.url}>{post.title}</a>
+      </li>
     {/each}
   {/each}
 </ul>
