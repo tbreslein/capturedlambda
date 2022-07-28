2ea5b89ff60389b7266d53c47c249712970c3f06
tbreslein <tommy.breslein@protonmail.com>
2022-07-28 10:48:51 +0200

    TWK|remove reddit link ++ twitter account was renamed

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -12,11 +12,8 @@
 </div>
 <footer class="fixed bottom-0 footer footer-center p-2 bg-base-300">
   <div class="grid grid-flow-col gap-4 text-secondary">
-    <a href="https://twitter.com/HedShodd" class={buttonClasses}> Twitter </a>
+    <a href="https://twitter.com/tommyb_dev" class={buttonClasses}> Twitter </a>
     <a href="https://github.com/tbreslein" class={buttonClasses}> Github </a>
-    <a href="https://www.reddit.com/user/Hedshodd" class={buttonClasses}>
-      Reddit
-    </a>
   </div>
   <p class="text-base-content">
     Copyright © 2022 - All right reserved by Tommy Breslein
