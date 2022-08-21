5b054a708ff29fedc80d58b1bf4401fe65988787
tbreslein <tommy.breslein@protonmail.com>
2022-08-21 22:07:06 +0200

    refactor: switch out Footer.svelte with Footer.tsx

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import { Component } from "solid-js";
+
+const buttonClasses = "btn btn-xs btn-ghost";
+const Footer: Component = () => (
+  <>
+    <div class="p-24">
+      <p />
+    </div>
+    <footer class="fixed bottom-0 footer footer-center p-2 bg-base-300">
+      <div class="grid grid-flow-col gap-4 text-secondary">
+        <a href="https://twitter.com/tommyb_dev" class={buttonClasses}>
+          Twitter
+        </a>
+        <a href="https://github.com/tbreslein" class={buttonClasses}>
+          Github
+        </a>
+      </div>
+      <p class="text-base-content">
+        Copyright © 2022 - All right reserved by Tommy Breslein
+      </p>
+    </footer>
+  </>
+);
+
+export default Footer;
