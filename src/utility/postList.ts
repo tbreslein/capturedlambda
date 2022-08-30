7edbf0cc2374aee5f9de551b21461771fe64df2e
tbreslein <tommy.breslein@protonmail.com>
2022-08-30 16:12:58 +0200

    fix(project): fix multiple type errors

--- src/utility/postList.ts
+++ src/utility/postList.ts
@@ -1,28 +1,28 @@
+import { MDXInstance } from "astro";
+
 export interface Frontmatter {
-  frontmatter: {
     title: string;
     date: string;
     draft?: boolean;
-  };
-  url: string;
+    url: string;
 }
 
 export type Post = {
-  title: string;
-  date: Date;
-  url: string;
-  draft: boolean;
+    title: string;
+    date: Date;
+    draft: boolean;
+    url: string;
 };
 
-export function getSortedPosts(posts: Frontmatter[]): Post[] {
-  return posts
-    .map((post) => {
-      return {
-        title: post.frontmatter.title,
-        date: new Date(post.frontmatter.date),
-        url: post.url,
-        draft: post.frontmatter.draft || false,
-      };
-    })
-    .sort((a, b) => b.date.valueOf() - a.date.valueOf());
+export function getSortedPosts(posts: MDXInstance<Frontmatter>[]): Post[] {
+    return posts
+        .map((post) => {
+            return {
+                title: post.frontmatter.title,
+                date: new Date(post.frontmatter.date),
+                url: post.url,
+                draft: post.frontmatter.draft || false,
+            };
+        })
+        .sort((a, b) => b.date.valueOf() - a.date.valueOf());
 }
