249a7984a498efb1107af759088652566c0c2240
tbreslein <tommy.breslein@protonmail.com>
2022-11-29 22:08:17 +0100

    add hero component to landing page and a bit of basic structure

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,14 +10,14 @@ import NavList from "./NavList";
 const Nav: Component = () => (
   <>
     <div class="hidden md:block">
-      <aside class="w-14 pl-0 md:w-36 fixed left-0 top-0 h-screen bg-neutral p-10">
+      <aside class="w-14 pl-0 md:w-36 fixed left-0 top-0 h-screen bg-base-300 p-10">
         <MainIcon />
         <NavList />
       </aside>
     </div>
 
     <div class="block md:hidden">
-      <nav class="navbar bg-neutral">
+      <nav class="navbar bg-base-300">
         <div class="flex-1">
           <MainIcon />
         </div>
