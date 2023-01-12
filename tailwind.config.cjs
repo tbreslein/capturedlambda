8fd14af70f17a8f4bdd881efdc27d9689cffdd8c
tbreslein <tommy.breslein@protonmail.com>
2023-01-12 16:20:22 +0100

    revamp theme ++ add theme switching

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,16 +7,29 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
-          primary: "#edddb8",
-          secondary: "#dcc080",
-          accent: "#dfb316",
-          neutral: "#292929",
-          "base-100": "#3d3d3d",
+        dark: {
+          primary: "#fffcf2",
+          secondary: "#ccc5b9",
+          // accent: "#ff5733",
+          accent: "#df4c2d",
+          neutral: "#403d39",
+          "base-100": "#252422",
           info: "#83a598",
           success: "#b8bb26",
           warning: "#d3869b",
           error: "#f87272",
+        },
+        light: {
+          primary: "#656e77",
+          secondary: "#cad4df",
+          // "accent": "#b2456e",
+          accent: "#df4c2d",
+          neutral: "#3b373b",
+          "base-100": "#dddbde",
+          info: "#1BC4F3",
+          success: "#1EAE9B",
+          warning: "#f9ec7e",
+          error: "#E83B6F",
         },
       },
     ],
